ce722e25a513bf5170ca89461b8e3f4da31042c9
Ahmed3117 <ahmedibra3117@gmail.com>
2024-02-23 17:00:13 +0200

    full crud and more touches added

--- todo/admin.py
+++ todo/admin.py
@@ -4,4 +4,13 @@ from todo.models import Todo
 
 # Register your models here.
 
-admin.site.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title','is_done')
+    def row_css_class(self, obj, index):
+        if obj.is_done == True:
+            return 'blue-row'
+        elif obj.is_done == False:
+            return 'red-row'
+        return ''
+
+admin.site.register(Todo, TodoAdmin)
